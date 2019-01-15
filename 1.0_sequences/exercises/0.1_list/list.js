d7b54f11b42c9a1389c735eeb4e3f3096763e6db
web-ronin <w_nik97@mail.ru>
2019-01-15 16:46:14 +0600

    add exercise

--- 1.0_sequences/exercises/0.1_list/list.js
+++ 1.0_sequences/exercises/0.1_list/list.js
@@ -1,33 +1,28 @@
 import { l, cons, head, tail, isEmpty, toString as listToString } from 'hexlet-pairs-data'; // eslint-disable-line
 
 // BEGIN (write your solution here)
-export const has = (list, value) => {
-  if (isEmpty(list) === true) {
-    return false;
-  } 
-  if (head(list) === value) {
-    return true;
-  } else {
-    return has(tail(list),value);
-  }
+
+export const has = (list, element) => {
+    if (head(list) === element) return true;
+    if (isEmpty(tail(list))) {
+        return tail(list) === element;
+    }
+    return has(tail(list), element);
 };
 
 export const reverse = (list) => {
-  const iter = (items, acc) => {
-    if (isEmpty(items) === true) {
-      return acc;
-    } else {
-      return iter(tail(items), cons(head(items), acc))
-    }
-  }
-  return iter(list, l());
-}
-
-export const concat = (list1, list2) => {
-  if (isEmpty(list1)) {
-    return list2;
-  }
-  return cons(head(list1), concat(tail(list1), list2));
+    if (isEmpty(list)) return l();
+    const iter = (count, acc) => {
+        if (isEmpty(count)) {
+            return acc;
+        }
+        return iter(tail(count), cons(head(count), acc));
+    };
+    return iter(list, l());
 };
 
+export const concat = (list1, list2) => {
+    if (isEmpty(list1)) return list2;
+    return cons(head(list1), concat(tail(list1), list2));
+};
 // END
