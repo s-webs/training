434acbed4af60a65f32ad079b1c9b87e45426216
web-ronin <w_nik97@mail.ru>
2019-02-12 06:01:42 +0600

    add exercise

---
+++ 1.1_ DataDrivenProgramming/0.5_objects/simpleCard.js
@@ -0,0 +1,14 @@
+// BEGIN (write your solution here)
+const make = (name, damage) => (message) => {
+    switch (message) {
+        case 'getName':
+        return name;
+        case 'damage':
+        return damage;
+        default:
+        return 'undefined method';
+    }
+};
+
+export default make;
+// END
